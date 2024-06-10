b6c5373f50d96e5f83ee78536a5fc449b43d3097
lin xu <helado_a@hotmail.com>
2024-06-10 23:07:40 +0200

    flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,4 +2,21 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
+
+  def new
+    @restaurant = Flat.new
+  end
+
+  def creat
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :picture_url, :description, :price_per_night, :number_of_guests)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root to: 'flats#index'
   resources :flats
+
 end
